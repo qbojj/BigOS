218e638b6f9fbb659b5623dfd561464d26e0575f
Jakub Janeczko <kuba@ev1.pl>
2025-03-21 23:20:27 +0100

    make buildsystem actually work

--- src/example/entry.c
+++ src/example/entry.c
@@ -1,9 +1,5 @@
-#include <debug/debug_stdio.h>
+#include <debug_stdio.h>
 
 void example_main() {
 	DEBUG_PUTS("hello OS\n");
-}
-
-void example_int_handler() {
-	DEBUG_PUTS("exception triggered\n");
 }
--- src/example/entry.s
+++ src/example/entry.s
@@ -12,9 +12,6 @@ start:
 	la gp, global_pointer
 .option pop
 
-	/* Reset satp */
-	csrw satp, zero
-
 	/* Setup stack */
 	la sp, stack_top
 
@@ -25,9 +22,6 @@ bss_clear:
 	sd zero, (t5)
 	addi t5, t5, 8
 	bltu t5, t6, bss_clear
-
-	la t0, example_int_handler
-	csrw mepc, t0
 
 	/* Jump to kernel! */
 	jal ra, example_main
