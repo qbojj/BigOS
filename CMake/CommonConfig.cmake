8582504a774f65741cf18ba7d6aeeffb0759e9af
Jakub Janeczko <kuba@ev1.pl>
2025-03-22 16:01:01 +0100

    fix building a bin

--- CMake/CommonConfig.cmake
+++ CMake/CommonConfig.cmake
@@ -50,6 +50,7 @@ endfunction()
 function(COMPILE_BINARY name)
     add_custom_command(
         TARGET ${name} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory "./$<CONFIG>"
         COMMAND ${CMAKE_OBJCOPY}
                     -O binary
                     "$<TARGET_FILE:${name}>"
