ff432790ec34481049da5375e946eb30b3f8ff8d
Jakub Janeczko <kuba@ev1.pl>
2025-03-29 00:45:54 +0100

    add LTO capability

--- src/example_sbi/startup.c
+++ src/example_sbi/startup.c
@@ -15,52 +15,61 @@ typedef void (*function_t)(void);
 
 // These symbols are defined by the linker script.
 // See linker.lds
-extern u8 __bss_start [[gnu::weak]];
-extern u8 __bss_end [[gnu::weak]];
+extern u8 __bss_start[];
+extern u8 __bss_end;
 
-extern function_t __preinit_array_start [[gnu::weak]];
-extern function_t __preinit_array_end [[gnu::weak]];
+extern function_t __preinit_array_start;
+extern function_t __preinit_array_end;
 
-extern function_t __init_array_start [[gnu::weak]];
-extern function_t __init_array_end [[gnu::weak]];
+extern function_t __init_array_start;
+extern function_t __init_array_end;
 
-extern function_t __fini_array_start [[gnu::weak]];
-extern function_t __fini_array_end [[gnu::weak]];
+extern function_t __fini_array_start;
+extern function_t __fini_array_end;
 
 extern int main(u32 hartid, const void* fdt);
 
 [[gnu::section(".init.enter"), gnu::naked]]
 void _enter(void) {
 	__asm__ volatile(".option push\n\t"
-	                 ".option norelax\n\t"
-	                 "la    gp, __global_pointer$\n\t"
-	                 ".option pop\n\t"
-	                 "la    sp, _sp\n\t"
-	                 "jal   zero, _start");
+					 ".option norelax\n\t"
+					 "la    gp, __global_pointer$\n\t"
+					 ".option pop\n\t"
+					 "la    sp, __stack_start\n\t"
+					 "jal   zero, _start");
 }
 
-[[noreturn, gnu::noinline]]
-void _Exit(int) {
-	while (1) wfi();
+[[gnu::section(".fini"), noreturn, gnu::noinline]]
+static void _Exit(int) {
+	while(1) wfi();
 }
 
-[[noreturn]]
-void _start(u32 hartid, const void* fdt) {
-	memset(&__bss_start, 0, &__bss_end - &__bss_start);
-
-	for (const function_t* entry = &__preinit_array_start; entry < &__preinit_array_end; ++entry) {
+[[gnu::section(".init")]]
+static void _call_constructors() {
+	for(const function_t* entry = &__preinit_array_start; entry < &__preinit_array_end; ++entry) {
 		(*entry)();
 	}
 
 	for (const function_t* entry = &__init_array_start; entry < &__init_array_end; ++entry) {
 		(*entry)();
 	}
+}
+
+[[gnu::section(".fini")]]
+static void _call_destructors() {
+	for(const function_t* entry = &__fini_array_start;
+		entry < &__fini_array_end; ++entry) {
+		(*entry)();
+	}
+}
+
+[[gnu::section(".init"), noreturn, gnu::used]]
+static void _start(u32 hartid, const void* fdt) {
+	memset(__bss_start, 0, &__bss_end - __bss_start);
+	_call_constructors();
 
 	int rc = main(hartid, fdt);
 
-	for (const function_t* entry = &__fini_array_start; entry < &__fini_array_end; ++entry) {
-		(*entry)();
-	}
-
+	_call_destructors();
 	_Exit(rc);
 }
