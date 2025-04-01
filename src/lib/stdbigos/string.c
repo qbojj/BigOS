c43b67858759c6ed2041d9d3439c40ea53f7eeea
Jakub Janeczko <kuba@ev1.pl>
2025-04-02 01:13:13 +0200

    tidy-up old files

--- src/lib/stdbigos/string.c
+++ src/lib/stdbigos/string.c
@@ -2,37 +2,29 @@
 #include <stdbigos/types.h>
 #include <stddef.h>
 
-/* This file contains low level library functions such as memcpy. As such it is
- * optimized for speed. We also define no-tree-loop-distribute-patterns to
- * prevent compiler from "optimize" those functions into a recursive call
- */
-#pragma GCC optimize("O3", "no-tree-loop-distribute-patterns")
-
 void* memcpy(void* restrict dest, const void* restrict src, size_t n) {
 	u8* d = dest;
 	const u8* s = src;
-	while (n--) {
-		*d++ = *s++;
-	}
+	while(n--) { *d++ = *s++; }
 	return dest;
 }
 
-void* memccpy(void* restrict dest, const void* restrict src, int ch, size_t n) {
+void* memccpy(void* restrict dest, const void* restrict src, int ch,
+			  size_t count) {
 	u8* d = dest;
 	const u8* s = src;
 
-	while (n--) {
+	while(count--) {
 		u8 cur = *s++;
 		*d++ = cur;
-		if (cur == ch)
-			return d;
+		if(cur == ch) return d;
 	}
 	return nullptr;
 }
 
 void* memset(void* dest, int val, size_t n) {
 	u8* d = dest;
-	while (n--) *d++ = val;
+	while(n--) *d++ = val;
 	return dest;
 }
 
@@ -43,25 +35,25 @@ void* memset_explicit(void* dest, int val, size_t n) {
 void* memmove(void* dest, const void* src, size_t n) {
 	u8* d = dest;
 	const u8* s = src;
-	if (d < s) {
-		while (n--) *d++ = *s++;
+	if(d < s) {
+		while(n--) *d++ = *s++;
 	} else {
 		u8* ld = d + (n - 1);
 		const u8* ls = s + (n - 1);
-		while (n--) *ld-- = *ls--;
+		while(n--) *ld-- = *ls--;
 	}
 	return dest;
 }
 
 size_t strlen(const char* str) {
 	size_t l = 0;
-	while (*str++) l++;
+	while(*str++) l++;
 	return l;
 }
 
 size_t strnlen(const char* str, size_t n) {
 	size_t l = 0;
-	while (n-- && *str++) l++;
+	while(n-- && *str++) l++;
 	return l;
 }
 
@@ -72,10 +64,11 @@ char* strcpy(char* restrict dest, const char* restrict src) {
 
 char* strncpy(char* restrict dest, const char* restrict src, size_t count) {
 	char* rest = (char*)memccpy(dest, src, '\0', count);
-	if (rest)
+	if(rest) {
 		memset(rest, '\0', count - (rest - dest));
-	else
+	} else {
 		dest[count - 1] = '\0'; // non conpliant, but better
+	}
 	return dest;
 }
 
@@ -84,11 +77,10 @@ char* strcat(char* restrict dest, const char* restrict src) {
 	return dest;
 }
 
-char* strncat(char* restrict dest, const char* restrict src, size_t n) {
-	size_t l = strnlen(dest, n);
-	char* end = memccpy(dest + l, src, '\0', n - l);
-	if (!end)
-		dest[n] = '\0';
+char* strncat(char* restrict dest, const char* restrict src, size_t count) {
+	size_t l = strnlen(dest, count);
+	char* end = memccpy(dest + l, src, '\0', count - l);
+	if(!end) dest[count] = '\0';
 	return dest;
 }
 
@@ -96,11 +88,9 @@ int memcmp(const void* lhs_, const void* rhs_, size_t n) {
 	const u8* lhs = lhs_;
 	const u8* rhs = rhs_;
 
-	while (n--) {
-		if (*lhs < *rhs)
-			return -1;
-		if (*lhs > *rhs)
-			return 1;
+	while(n--) {
+		if(*lhs < *rhs) return -1;
+		if(*lhs > *rhs) return 1;
 		lhs++;
 		rhs++;
 	}
@@ -108,12 +98,11 @@ int memcmp(const void* lhs_, const void* rhs_, size_t n) {
 	return 0;
 }
 
-void* memchr(const void* src_, int ch, size_t n) {
-	const u8* s = src_;
+void* memchr(const void* ptr, int ch, size_t n) {
+	const u8* s = ptr;
 
-	while (n--) {
-		if (*s == ch)
-			return (void*)s;
+	while(n--) {
+		if(*s == ch) return (void*)s;
 		s++;
 	}
 
@@ -121,38 +110,21 @@ void* memchr(const void* src_, int ch, size_t n) {
 }
 
 int strcmp(const char* lhs, const char* rhs) {
-	while (true) {
-		if (*lhs < *rhs)
-			return -1;
-		if (*lhs > *rhs)
-			return 1;
-		if (!*lhs)
-			return 0;
-		lhs++;
-		rhs++;
-	}
+	int ret = 0;
+	while(!ret) { ret = (int)*lhs++ - (int)*rhs++; }
+	return ret;
 }
 
 int strncmp(const char* lhs, const char* rhs, size_t n) {
-	while (n--) {
-		if (*lhs < *rhs)
-			return -1;
-		if (*lhs > *rhs)
-			return 1;
-		if (!*lhs)
-			return 0;
-		lhs++;
-		rhs++;
-	}
-	return 0;
+	int ret = 0;
+	while(n-- && !ret) { ret = (int)*lhs++ - (int)*rhs++; }
+	return ret;
 }
 
 char* strchr(const char* str, int ch) {
-	while (true) {
-		if (*str == ch)
-			return (char*)str;
-		if (!*str)
-			return nullptr;
+	while(true) {
+		if(*str == ch) return (char*)str;
+		if(!*str) return nullptr;
 		str++;
 	}
 }
@@ -160,42 +132,38 @@ char* strchr(const char* str, int ch) {
 char* strrchr(const char* str, int ch) {
 	char* res = nullptr;
 
-	while (*str) {
-		if (*str == ch)
-			res = (char*)str;
+	while(*str) {
+		if(*str == ch) res = (char*)str;
 		str++;
 	}
 
-	if ('\0' == ch)
-		res = (char*)str;
+	if('\0' == ch) res = (char*)str;
 	return res;
 }
 
 char* strstr(const char* str, const char* substr) {
 	size_t l1 = strlen(str);
 	size_t l2 = strlen(substr);
-	if (l2 > l1)
-		return nullptr;
+	if(l2 > l1) return nullptr;
 
-	for (size_t i = 0; i <= l2 - l1; ++i) {
-		if (memcmp(str + i, substr, l2) == 0)
-			return (char*)str + i;
+	for(size_t i = 0; i <= l2 - l1; ++i) {
+		if(memcmp(str + i, substr, l2) == 0) return (char*)str + i;
 	}
 	return nullptr;
 }
 
-size_t strspn(const char* dest, const char* src) {
+size_t strspn(const char* dest, const char* chars) {
 	size_t l = 0;
-	while (*dest && strchr(src, *dest)) {
+	while(*dest && strchr(chars, *dest)) {
 		l++;
 		dest++;
 	}
 	return l;
 }
 
-size_t strcspn(const char* dest, const char* src) {
+size_t strcspn(const char* dest, const char* chars) {
 	size_t l = 0;
-	while (*dest && !strchr(src, *dest)) {
+	while(*dest && !strchr(chars, *dest)) {
 		l++;
 		dest++;
 	}
@@ -203,9 +171,8 @@ size_t strcspn(const char* dest, const char* src) {
 }
 
 char* strpbrk(const char* dest, const char* breakset) {
-	while (*dest) {
-		if (strchr(breakset, *dest))
-			return (char*)dest;
+	while(*dest) {
+		if(strchr(breakset, *dest)) return (char*)dest;
 		dest++;
 	}
 	return nullptr;
