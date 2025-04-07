db75ee914c8e819785080a175bed09ff08e97073
Jakub Janeczko <kuba@ev1.pl>
2025-04-08 01:23:00 +0200

    format

--- src/bootloader/test.c
+++ src/bootloader/test.c
@@ -1,9 +1,8 @@
 #include <efi.h>
 #include <efilib.h>
 
-
-EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"Hello, EFI!\n");
-    return EFI_SUCCESS;
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
+	InitializeLib(ImageHandle, SystemTable);
+	Print(L"Hello, EFI!\n");
+	return EFI_SUCCESS;
 }
