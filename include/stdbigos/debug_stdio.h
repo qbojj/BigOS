af4c79c0cbed44ec560b9e8e0f8d64036d89d111
Jakub Janeczko <kuba@ev1.pl>
2025-05-10 21:25:30 +0200

    remove unneeded macros

--- include/stdbigos/debug_stdio.h
+++ include/stdbigos/debug_stdio.h
@@ -1,24 +1,31 @@
 #ifndef _DEBUG_STDIO_
 #define _DEBUG_STDIO_
 
+#include <stdarg.h>
 #include <stdio.h>
 
 #ifndef NDEBUG
 
-	#define DEBUG_PUTC(c) fputc(c, stderr)
-	#define DEBUG_PUTS(s) fputs(s, stderr)
-	#define DEBUG_PRINTF(fmt, ...)                      \
-		fprintf(stderr, fmt __VA_OPT__(, ) __VA_ARGS__)
+static inline void dputc(int c) {
+	putchar(c);
+}
+static inline void dputs(char* s) {
+	puts(s);
+}
+[[gnu::format(printf, 1, 2)]]
+static inline void dprintf(char* fmt, ...) {
+	va_list va;
+	va_start(va);
+	vprintf(fmt, va);
+	va_end(va);
+}
 
 #else
 
-// for argument checking
-[[gnu::format(printf, 1, 2)]] static inline void noop_printf(const char*, ...) {
-}
-
-	#define DEBUG_PUTC(c)
-	#define DEBUG_PUTS(s)
-	#define DEBUG_PRINTF(fmt, ...) noop_printf(fmt __VA_OPT__(, ) __VA_ARGS__)
+static inline void dputc(int c) {}
+static inline void dputs(char* s) {}
+[[gnu::format(printf, 1, 2)]]
+static inline void dprintf(char*, ...) {}
 
 #endif
 #endif
