5cd913deea59d6597c696ee0a67fcaff2654b245
GoscZnickiem <maciek.6098@gmail.com>
2025-05-19 00:09:51 +0200

    add simple kernel loading and FDT retrieval

--- src/bootloader/test.c
+++
@@ -1,8 +0,0 @@
-#include <efi.h>
-#include <efilib.h>
-
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
-	InitializeLib(ImageHandle, SystemTable);
-	Print(L"Hello, EFI!\n");
-	return EFI_SUCCESS;
-}
