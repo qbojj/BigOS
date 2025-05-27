1b74bb70d9d3616e2caa02e0c64b1dc2b512578d
GoscZnickiem <maciek.6098@gmail.com>
2025-05-27 16:57:27 +0200

    finish partition handling

--- src/bootloader/partition.h
+++ src/bootloader/partition.h
@@ -12,15 +12,17 @@
 #include <efi.h>
 
 typedef struct {
+	UINTN flags;
 	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* file_system;
 	EFI_FILE_PROTOCOL* root;
 	EFI_FILE_SYSTEM_INFO* file_system_info;
-	UINTN file_system_info_size;
 	EFI_DEVICE_PATH_PROTOCOL* device_path;
 } partition_t;
 
 EFI_STATUS partition_table_create();
 void partition_table_free();
-void partition_data_print(partition_t* partition);
+
+void partition_print(partition_t* partition);
+void partition_table_print();
 
 #endif // !BIGOS_BOOTLOADER_PARTITION
