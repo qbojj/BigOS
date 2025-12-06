be8de7e0c442504f12f602b6164d594a75507347
Bartlomiej Stefanski <bartex00001@gmail.com>
2025-12-06 14:18:24 +0100

    [#52] CMake: Add target flag for clang

--- CMake/toolchains/riscv64-clang.cmake
+++ CMake/toolchains/riscv64-clang.cmake
@@ -8,3 +8,6 @@ set(RISCV_CLANG_TOOLCHAIN_INCLUDED TRUE)
 include(${CMAKE_CURRENT_LIST_DIR}/riscv64-common.cmake)
 
 set(CMAKE_C_COMPILER clang)
+
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} --target=riscv64-unknown-elf")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=riscv64-unknown-elf")
