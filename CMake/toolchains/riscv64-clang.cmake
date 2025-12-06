f7de5e6a260c6bfea356d8c5389ae75180991603
Bartlomiej Stefanski <bartex00001@gmail.com>
2025-12-06 14:19:11 +0100

    [#52] CMake: When using clang toolchain set clang as assembly compiler

--- CMake/toolchains/riscv64-clang.cmake
+++ CMake/toolchains/riscv64-clang.cmake
@@ -7,6 +7,7 @@ set(RISCV_CLANG_TOOLCHAIN_INCLUDED TRUE)
 
 include(${CMAKE_CURRENT_LIST_DIR}/riscv64-common.cmake)
 
+set(CMAKE_ASM_COMPILER clang)
 set(CMAKE_C_COMPILER clang)
 
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} --target=riscv64-unknown-elf")
