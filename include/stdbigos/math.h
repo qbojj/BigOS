5e8bc266c7fbb76f73a9ebc52a6004e230d9de69
Jakub Janeczko <kuba@ev1.pl>
2026-03-09 23:18:44 +0100

    unifi alignment math

--- include/stdbigos/math.h
+++ include/stdbigos/math.h
@@ -3,15 +3,15 @@
 
 #include "types.h"
 
+#define POW2(x) (1ull << (x))
+
 #define ALIGN_DOWN(x, align)     ((x) / (align) * (align))
-#define ALIGN_DOWN_POW2(x, pow2) ((x) & ~((1ull << pow2) - 1))
+#define ALIGN_DOWN_POW2(x, pow2) ALIGN_DOWN(x, POW2(pow2))
 #define ALIGN_UP(x, align)       ALIGN_DOWN((x) + (align) - 1, align)
-#define ALIGN_UP_POW2(x, pow2)   ALIGN_DOWN_POW2((x) + (1ull << (pow2)) - 1, pow2)
+#define ALIGN_UP_POW2(x, pow2)   ALIGN_UP(x, POW2(pow2))
 
-#define IS_ALIGNED(ptr, align)   ((reg_t)(x) == ALIGN_DOWN((reg_t)(x), (align))
-#define IS_ALIGNED_POW2(ptr, po2)   ((reg_t)(x) == ALIGN_DOWN_POW2((reg_t)(x), (align))
-
-#define POW2(x) (1 << (x))
+#define IS_ALIGNED(ptr, align)     ((reg_t)(x) == ALIGN_DOWN((reg_t)(x), align))
+#define IS_ALIGNED_POW2(ptr, pow2) ((reg_t)(x) == ALIGN_DOWN_POW2((reg_t)(x), pow2))
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
