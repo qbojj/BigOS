5738686a6e23e2cc5d72a5d14e13b38a037e36d7
Jakub Janeczko <kuba@ev1.pl>
2026-03-31 08:06:44 +0200

    adjust startup linker layout and string visibility

--- include/relocations/reloc.h
+++ include/relocations/reloc.h
@@ -1,6 +1,31 @@
 #ifndef RELOCATIONS_H_
 #define RELOCATIONS_H_
 
-bool self_relocate();
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef void (*reloc_function_t)(void);
+
+typedef struct {
+	const reloc_function_t* preinit_array;
+	uintptr_t preinit_count;
+	reloc_function_t init;
+	const reloc_function_t* init_array;
+	uintptr_t init_count;
+	reloc_function_t fini;
+	const reloc_function_t* fini_array;
+	uintptr_t fini_count;
+} reloc_init_arrays_t;
+
+typedef struct {
+	void* load_addr;
+	const void* rela;
+	uintptr_t relasz;
+	uintptr_t relaent;
+	reloc_init_arrays_t init_arrays;
+} reloc_dynamic_info_t;
+
+bool reloc_load_dynamic_info(reloc_dynamic_info_t* info);
+bool self_relocate(const reloc_dynamic_info_t* info);
 
 #endif
--- src/lib/relocations/reloc.c
+++ src/lib/relocations/reloc.c
@@ -20,36 +20,64 @@ static inline Elf64_Dyn* elf_get_dynamic_runtime() {
 	return dynamic;
 }
 
-bool self_relocate() {
-	Elf64_Dyn* dynamic = elf_get_dynamic_runtime();
-	u8* load_addr = (u8*)((uintptr_t)dynamic - elf_get_dynamic_linktime());
+bool reloc_load_dynamic_info(reloc_dynamic_info_t* info) {
+	if (!info) {
+		return false;
+	}
 
-	Elf64_Xword relasz = 0;
-	Elf64_Xword relaent = 0;
-	const u8* rela = nullptr;
+	*info = (reloc_dynamic_info_t){0};
+
+	Elf64_Dyn* dynamic = elf_get_dynamic_runtime();
+	info->load_addr = (void*)((uintptr_t)dynamic - elf_get_dynamic_linktime());
+
+	u8* ld_addr = (u8*)info->load_addr;
 
 	for (; dynamic->d_tag != DT_NULL; dynamic++) {
 		switch (dynamic->d_tag) {
-		case DT_RELA:    rela = load_addr + dynamic->d_un.d_ptr; break;
-		case DT_RELASZ:  relasz = dynamic->d_un.d_val; break;
-		case DT_RELAENT: relaent = dynamic->d_un.d_val; break;
-		default:         break;
+		case DT_PREINIT_ARRAY:
+			info->init_arrays.preinit_array = (const reloc_function_t*)(ld_addr + dynamic->d_un.d_ptr);
+			break;
+		case DT_PREINIT_ARRAYSZ:
+			info->init_arrays.preinit_count = dynamic->d_un.d_val / sizeof(reloc_function_t);
+			break;
+		case DT_INIT: info->init_arrays.init = (reloc_function_t)(ld_addr + dynamic->d_un.d_ptr); break;
+		case DT_INIT_ARRAY:
+			info->init_arrays.init_array = (const reloc_function_t*)(ld_addr + dynamic->d_un.d_ptr);
+			break;
+		case DT_INIT_ARRAYSZ: info->init_arrays.init_count = dynamic->d_un.d_val / sizeof(reloc_function_t); break;
+		case DT_FINI:         info->init_arrays.fini = (reloc_function_t)(ld_addr + dynamic->d_un.d_ptr); break;
+		case DT_FINI_ARRAY:
+			info->init_arrays.fini_array = (const reloc_function_t*)(ld_addr + dynamic->d_un.d_ptr);
+			break;
+		case DT_FINI_ARRAYSZ: info->init_arrays.fini_count = dynamic->d_un.d_val / sizeof(reloc_function_t); break;
+		case DT_RELA:         info->rela = ld_addr + dynamic->d_un.d_ptr; break;
+		case DT_RELASZ:       info->relasz = dynamic->d_un.d_val; break;
+		case DT_RELAENT:      info->relaent = dynamic->d_un.d_val; break;
+		default:              break;
 		}
 	}
 
-	if (!rela)
-		return relaent == 0;
+	return true;
+}
 
-	if (relaent == 0)
-		return rela == nullptr;
+bool self_relocate(const reloc_dynamic_info_t* info) {
+	if (!info) {
+		return false;
+	}
 
-	for (Elf64_Xword off = 0; off < relasz; off += relaent) {
-		const Elf64_Rela* cur_rel = (const Elf64_Rela*)(rela + off);
+	if (!info->rela)
+		return info->relaent == 0;
+
+	if (info->relaent == 0)
+		return info->rela == nullptr;
+
+	for (uintptr_t off = 0; off < info->relasz; off += info->relaent) {
+		const Elf64_Rela* cur_rel = (const Elf64_Rela*)((u8*)info->rela + off);
 		switch (ELF64_R_TYPE(cur_rel->r_info)) {
 		case R_RISCV_NONE: break;
 		case R_RISCV_RELATIVE:
-			void** addr = (void**)(load_addr + cur_rel->r_offset);
-			*addr = load_addr + cur_rel->r_addend;
+			void** addr = (void**)((u8*)info->load_addr + cur_rel->r_offset);
+			*addr = (u8*)info->load_addr + cur_rel->r_addend;
 			break;
 		default:
 			// unknown relocation type
